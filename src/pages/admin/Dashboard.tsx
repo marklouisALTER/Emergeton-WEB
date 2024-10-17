0cae8feb84bd55f41b77c138436c1801f1705940
Mark Louis Bernardo <marklouisbernando318@gmail.com>
2024-10-17 20:45:19 +0800

    feat: added cards

--- src/pages/admin/Dashboard.tsx
+++ src/pages/admin/Dashboard.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
+import { MdLocalPolice } from "react-icons/md";
+import { MdFireTruck } from "react-icons/md";
+import { MdHealthAndSafety } from "react-icons/md";
 
 const Dashboard:React.FC = () => {
   return (
     <section className='p-5 md:pl-5'>
       <h1 className='font-primary text-black/80 text-xl font-semibold'>Admin Dashboard</h1>
-      <div className='grid grid-cols-2 h-screen gap-5 py-5'>  
+      <div className='grid grid-cols-1 sm:grid-cols-2 h-screen gap-5 py-5'>  
         <div className='py-5'>
           <div className='mb-5'>
             <h3 className='font-secondary text-black/80 font-medium'>Emergency Map</h3>
@@ -42,13 +45,50 @@ const Dashboard:React.FC = () => {
         </div>
         <div className='w-full rounded-md p-5 shadow-md h-full border-2 border-gray-300'>
           <h3 className='font-secondary text-black/80 font-medium'>Unit Available</h3>
-          <div className='mt-5 grid grid-cols-3 gap-5'>
-            <div className='rounded-md w-full h-[5rem] bg-primary'>
+          <div className='mt-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5'>
+            <div className='rounded-md w-full bg-primary mt-5 p-5 relative'>
+              <div className='w-full flex items-end justify-end mt-5'> 
+                <span className='font-primary text-2xl font-extrabold text-white text-right'>20</span>
+              </div>
+              <div className='absolute top-[-1rem] flex items-center justify-center left-2 w-10 h-10 bg-blue-600 rounded-md'>
+                  <MdLocalPolice className='text-white text-xl'/>
+              </div>
+              <div className='absolute top-[-0.4rem] flex items-center justify-center right-2 px-3 py-1 bg-blue-700 rounded-md'>
+                <p className='text-white text-[11px]'>Police</p>
+              </div>
             </div>
-            <div className='rounded-md w-full h-[5rem] bg-primary'>
+            <div className='rounded-md w-full bg-red-600/80 mt-5 p-5 relative'>
+              <div className='w-full flex items-end justify-end mt-5'> 
+                <span className='font-primary text-2xl font-extrabold text-white text-right'>5</span>
+              </div>
+              <div className='absolute top-[-1rem] flex items-center justify-center left-2 w-10 h-10 bg-red-600 rounded-md'>
+                  <MdFireTruck className='text-white text-xl'/>
+              </div>
+              <div className='absolute top-[-0.4rem] flex items-center justify-center right-2 px-3 py-1 bg-red-700 rounded-md'>
+                <p className='text-white text-[11px]'>Fire Truck</p>
+              </div>
             </div>
-            <div className='rounded-md w-full h-[5rem] bg-primary'>
+            <div className='rounded-md w-full bg-green-600/80 mt-5 p-5 relative'>
+              <div className='w-full flex items-end justify-end mt-5'> 
+                <span className='font-primary text-2xl font-extrabold text-white text-right'>5</span>
+              </div>
+              <div className='absolute top-[-1rem] flex items-center justify-center left-2 w-10 h-10 bg-green-600 rounded-md'>
+                  <MdHealthAndSafety className='text-white text-xl'/>
+              </div>
+              <div className='absolute top-[-0.4rem] flex items-center justify-center right-2 px-3 py-1 bg-green-700 rounded-md'>
+                <p className='text-white text-[11px]'>Health care</p>
+              </div>
             </div>
+          </div>
+          <div className='w-full mt-5'>
+              <div className='w-full bg-primary h-[15rem] rounded-md'>
+              </div>
+              <div className='grid grid-cols-2 gap-5 mt-5'>
+                <div className='w-full bg-primary h-[10rem] rounded-md'>
+                </div>
+                <div className='w-full bg-primary h-[10rem] rounded-md'>
+                </div>
+              </div>
           </div>
         </div>
       </div>
